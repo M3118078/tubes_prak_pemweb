87e8cfb8aac2b31c065e8e32fd050a6e3d60c22a
FerdianPio <priambodho.ferdian@gmail.com>
2020-12-22 21:23:23 +0700

    resolve log sesion

--- home.php
+++ home.php
@@ -2,10 +2,10 @@
 	<!-- Banner 1-->
 
 	<div class="banner">
-		<div class="banner_background"><img src="images/banner_background.jpg" alt=""></div>
+		<div class="banner_2_background"><img src="images/banner_background.jpg" alt=""></div>
 		<div class="container fill_height">
 			<div class="row fill_height">
-				<div class="banner_product_image"><img src="images/banner_product.png" alt=""></div>
+				
 				<div class="col-lg-5 offset-lg-4 fill_height">
 					<div class="banner_content">
 						<h1 class="banner_text">Belanja Kebutuhan Gadgetmu Disini</h1>
